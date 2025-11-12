5ad4b91c0327f2dc8b0bfc87e63f74265383aa52
iamdarkle <tomasromero@outlook.com>
2025-11-12 03:13:42 +0100

    feat: admin frontend extender

---
+++ js/src/admin/extend.tsx
@@ -0,0 +1,33 @@
+import Extend from 'flarum/common/extenders';
+
+export default [
+  new Extend.Admin()
+    .setting({
+      setting: 'flectar-turnstile.site_key',
+      type: 'text',
+      label: flarum.translator.trans('flectar-turnstile.admin.settings.site_key'),
+      help: flarum.translator.trans('flectar-turnstile.admin.settings.help_text', {
+        a: <a href="https://dash.cloudflare.com/?to=/:account/turnstile" target="_blank" rel="noopener" />,
+      }),
+    })
+    .setting({
+      setting: 'flectar-turnstile.secret_key',
+      type: 'text',
+      label: flarum.translator.trans('flectar-turnstile.admin.settings.secret_key'),
+    })
+    .setting({
+      setting: 'flectar-turnstile.signup',
+      type: 'bool',
+      label: flarum.translator.trans('flectar-turnstile.admin.settings.signup'),
+    })
+    .setting({
+      setting: 'flectar-turnstile.signin',
+      type: 'bool',
+      label: flarum.translator.trans('flectar-turnstile.admin.settings.signin'),
+    })
+    .setting({
+      setting: 'flectar-turnstile.forgot',
+      type: 'bool',
+      label: flarum.translator.trans('flectar-turnstile.admin.settings.forgot'),
+    }),
+];
