4ab9c22033d06bd33c76b4cb93afc359c5b05508
iamdarkle <tomasromero@outlook.com>
2025-11-12 03:26:51 +0100

    fix import

--- js/src/admin/extend.tsx
+++ js/src/admin/extend.tsx
@@ -1,3 +1,4 @@
+import app from 'flarum/admin/app';
 import Extend from 'flarum/common/extenders';
 
 export default [
@@ -5,29 +6,29 @@ export default [
     .setting(() => ({
       setting: 'flectar-turnstile.site_key',
       type: 'text',
-      label: flarum.translator.trans('flectar-turnstile.admin.settings.site_key'),
-      help: flarum.translator.trans('flectar-turnstile.admin.settings.help_text', {
+      label: app.translator.trans('flectar-turnstile.admin.settings.site_key'),
+      help: app.translator.trans('flectar-turnstile.admin.settings.help_text', {
         a: <a href="https://dash.cloudflare.com/?to=/:account/turnstile" target="_blank" rel="noopener" />,
       }),
     }))
     .setting(() => ({
       setting: 'flectar-turnstile.secret_key',
       type: 'text',
-      label: flarum.translator.trans('flectar-turnstile.admin.settings.secret_key'),
+      label: app.translator.trans('flectar-turnstile.admin.settings.secret_key'),
     }))
     .setting(() => ({
       setting: 'flectar-turnstile.signup',
       type: 'bool',
-      label: flarum.translator.trans('flectar-turnstile.admin.settings.signup'),
+      label: app.translator.trans('flectar-turnstile.admin.settings.signup'),
     }))
     .setting(() => ({
       setting: 'flectar-turnstile.signin',
       type: 'bool',
-      label: flarum.translator.trans('flectar-turnstile.admin.settings.signin'),
+      label: app.translator.trans('flectar-turnstile.admin.settings.signin'),
     }))
     .setting(() => ({
       setting: 'flectar-turnstile.forgot',
       type: 'bool',
-      label: flarum.translator.trans('flectar-turnstile.admin.settings.forgot'),
+      label: app.translator.trans('flectar-turnstile.admin.settings.forgot'),
     })),
 ];
